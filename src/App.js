0b40cfd513e19dc127f266c7c183bc6892b2bc31
Isha1711 <ishakhatri.imscit20@gmail.com>
2024-02-23 14:56:14 +0530

    app

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 export default function App() {
   return (
     <>
-    <b>
-      hare krishna
-      </b>
+    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}>
